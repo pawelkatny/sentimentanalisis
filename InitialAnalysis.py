4384944aef5cf8055f3fca2843e03a7ba0c17eb9
pawelkatny <pawel.katny@gmail.com>
2020-01-16 22:21:59 +0100

    16/01/2020 Adds: - InitialAnalysis: 	a) occurence_counter() - creates and returns also an array of hashtags count for each tweet - Loader: 	b) adds text loading bar

--- InitialAnalysis.py
+++ InitialAnalysis.py
@@ -1,32 +1,28 @@
 import re
-import pandas as pd
-from patterns import patterns_list #regex patterns list
+from Patterns import patterns_list as patterns #regex patterns list
 import Loader as ld  # prints current progress
-
+from Functions import *
 
 class InitialAnalysis():
-    patterns = patterns_list
 
     def __init__(self, data):
         self.data = data
         self.tweets = data['Tweet']
         self.size = len(data)
 
-    def occurence_counter(self, key):  # emots, hash counter - returns descending sorted array
+    def occurence_counter(self, key):  # emots, hash etc counter - returns descending sorted array
                                         # and panda data frame wich stores hash count for every tweet
-        def create_data_frame(sorted_arr):
-            data_frame_sorted = pd.DataFrame(sorted_arr)
-            return data_frame_sorted
-
         key_list = []
+        occurence_counter = []
 
         loader = ld.Loader(self.size)
 
         for tweet in self.tweets:
-            list = re.findall(patterns_list[key], tweet)
-            list = [hash.lower() for hash in list] #changes hash text to lowercase to avoid case sensivity
+            list = re.findall(patterns[key], tweet)
 
             if list:
+                list = [hash.lower() for hash in list]  # changes hash text to lowercase to avoid case sensivity
+                occurence_counter.append(len(list))
                 for ele in list:
                     counter = 0
                     if len(ele) != 1:
@@ -36,12 +32,15 @@ class InitialAnalysis():
                                 counter = 1
                         if counter == 0:
                             key_list.append([ele, 1])
+            else:
+                occurence_counter.append(0)
 
             loader.loading()
 
         sorted_key_list = sorted(key_list, key=lambda x: x[1], reverse=True)
+        sorted_key_list = pd.DataFrame(sorted_key_list)
 
-        return create_data_frame(sorted_key_list)
+        return sorted_key_list, occurence_counter
 
 
     def tweets_len(self): #returns data frame tweet length
--- Loader.py
+++ Loader.py
@@ -7,11 +7,13 @@ class Loader():
         self.size = size
         self.current = 0
 
-    def loading(self):
+    def loading(self, text = ''):
         self.current += 1
         percent = int(self.current/self.size *100)
-        time.sleep(0.001)
-        sys.stdout.write('\rProgress ' + str(percent) + '%')
+        bar = int(round(20 * self.current/self.size))
+        # time.sleep(0.0001)
+        text_bar = '[' + '*'*bar + ' '*(20-bar) + ']'
+        sys.stdout.write('\rProcessing data: ' + text_bar + ' - ' + str(percent) + '%')
         sys.stdout.flush()
         if percent == 100:
             print('\nFinished.\n')
